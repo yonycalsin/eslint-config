00bfd8e5979243c23db6f8fe02013669f0f5c892
Yony <58490737+yonycalsin@users.noreply.github.com>
2022-05-16 23:24:09 -0500

    feat: add eslint configs for react and node

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+  configs: {
+    react: {
+      plugins: ["simple-import-sort"],
+      rules: {
+        "simple-import-sort/imports": [
+          "error",
+          {
+            groups: [
+              // Side effect imports.
+              ["^\\u0000"],
+              // Packages. `react` related packages come first.
+              ["^react", "^@?\\w"],
+              // Internal modules.
+              ["^~/?\\w"],
+              // Parent imports. Put `..` last.
+              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+              // Style imports.
+              ["^.+\\.css$"],
+            ],
+          },
+        ],
+      },
+    },
+    node: {
+      plugins: ["simple-import-sort"],
+      rules: {
+        "simple-import-sort/imports": [
+          "error",
+          {
+            "groups": [
+              // Side effect imports.
+              ["^\\u0000"],
+              // Packages. related packages come first.
+              ["^lodash", "^@?\\w"],
+              // Internal modules.
+              ["^~/?\\w"],
+              // Parent imports. Put `..` last.
+              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"]
+            ]
+          }
+        ],
+      }
+    },
+  },
+};
