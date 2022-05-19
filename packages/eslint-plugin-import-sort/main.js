3cf0b792768248a8fda438af9c89749cd11fcea2
Yony Calsin <58490737+yonycalsin@users.noreply.github.com>
2022-05-18 23:59:48 -0500

    chore: setup prettier

--- packages/eslint-plugin-import-sort/main.js
+++ packages/eslint-plugin-import-sort/main.js
@@ -1,52 +1,52 @@
 module.exports = {
   configs: {
     react: {
-      plugins: ["simple-import-sort"],
+      plugins: ['simple-import-sort'],
       rules: {
-        "simple-import-sort/imports": [
-          "error",
+        'simple-import-sort/imports': [
+          'error',
           {
             groups: [
               // Side effect imports.
-              ["^\\u0000"],
+              ['^\\u0000'],
               // Packages. `react` related packages come first.
-              ["^react", "^@?\\w"],
+              ['^react', '^@?\\w'],
               // Internal modules.
-              ["^~/?\\w"],
+              ['^~/?\\w'],
               // Parent imports. Put `..` last.
-              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
               // Other relative imports. Put same-folder imports and `.` last.
-              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
               // Style imports.
-              ["^.+\\.css$"],
+              ['^.+\\.css$'],
             ],
           },
         ],
-        "simple-import-sort/exports": "error",
+        'simple-import-sort/exports': 'error',
       },
     },
     node: {
-      plugins: ["simple-import-sort"],
+      plugins: ['simple-import-sort'],
       rules: {
-        "simple-import-sort/imports": [
-          "error",
+        'simple-import-sort/imports': [
+          'error',
           {
             groups: [
               // Side effect imports.
-              ["^\\u0000"],
+              ['^\\u0000'],
               // Packages. related packages come first.
-              ["^lodash", "^@?\\w"],
+              ['^lodash', '^@?\\w'],
               // Internal modules.
-              ["^~/?\\w"],
+              ['^~/?\\w'],
               // Parent imports. Put `..` last.
-              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
               // Other relative imports. Put same-folder imports and `.` last.
-              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
             ],
           },
         ],
-        "simple-import-sort/exports": "error",
+        'simple-import-sort/exports': 'error',
       },
     },
   },
-};
+}
