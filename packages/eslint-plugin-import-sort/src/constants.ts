ffe9e13f2788a10cb07748c651f1c02ec7effc85
Yony <58490737+yonycalsin@users.noreply.github.com>
2022-10-07 19:42:57 -0500

    feat: support modules (#8)
    * feat: support modules
    
    * docs(changeset): support modules

---
+++ packages/eslint-plugin-import-sort/src/constants.ts
@@ -0,0 +1,49 @@
+const DEFAULT_MODULES = [
+  'typings',
+  'mock-server',
+  'tests',
+  'services',
+  'themes',
+  'router',
+  'hooks',
+  'screens',
+  'layouts',
+  'containers',
+  'components',
+  'utils',
+  'constants',
+]
+
+const ASSETS_MAP = {
+  CSS_MODULES: '^.+\\.css$',
+  ASSETS_FOLDER: '^assets',
+}
+
+const ASSETS_GROUP = Object.values(ASSETS_MAP)
+
+const RELATIVES_GROUP = [
+  // Parent imports. Put `..` last.
+  '^\\.\\.(?!/?$).*\\u0000$',
+  '^\\.\\./?$.*\\u0000$',
+  '^\\.\\.(?!/?$)',
+  '^\\.\\./?$',
+  // Other relative imports. Put same-folder imports and `.` last.
+  '^\\./(?=.*/)(?!/?$).*\\u0000$',
+  '^\\.(?!/?$).*\\u0000$',
+  '^\\./?$.*\\u0000$',
+  '^\\./(?=.*/)(?!/?$)',
+  '^\\.(?!/?$)',
+  '^\\./?$',
+]
+
+const DEPENDENCIES_MAP = {
+  REACT_TYPES: '^react\\u0000$',
+  DEPENDENCIES_TYPES: `^@?\\w.*\\u0000$`,
+  SIDE_EFFECTS: '^\\u0000',
+  REACT: '^react$',
+  DEPENDENCIES: `^@?\\w`,
+}
+
+const DEPENDENCIES_GROUP = Object.values(DEPENDENCIES_MAP)
+
+export { ASSETS_GROUP, ASSETS_MAP, RELATIVES_GROUP, DEPENDENCIES_MAP, DEFAULT_MODULES, DEPENDENCIES_GROUP }
--- packages/eslint-plugin-import-sort/src/react.ts
+++ packages/eslint-plugin-import-sort/src/react.ts
@@ -1,19 +1,11 @@
+import { ASSETS_GROUP, DEPENDENCIES_GROUP, RELATIVES_GROUP } from './constants'
+
+const GROUPS = [ASSETS_GROUP, DEPENDENCIES_GROUP, ['^~/?\\w'], RELATIVES_GROUP]
+
 export default {
   plugins: ['simple-import-sort'],
   rules: {
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          ['^\\u0000'],
-          ['^react', '^@?\\w'],
-          ['^~/?\\w'],
-          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-          ['^.+\\.css$'],
-        ],
-      },
-    ],
+    'simple-import-sort/imports': ['error', { groups: GROUPS }],
     'simple-import-sort/exports': 'error',
   },
 }
