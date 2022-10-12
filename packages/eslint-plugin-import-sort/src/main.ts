8dbee5a6eef023241532dabd4a5dc3ada22f80f3
Yony Calsin <helloyonycalsin@gmail.com>
2022-10-11 22:09:50 -0500

    feat: add typescript-react-imports rule

--- packages/eslint-plugin-import-sort/src/main.ts
+++ packages/eslint-plugin-import-sort/src/main.ts
@@ -1,10 +1,14 @@
 import node from './node'
 import react from './react'
+import imports from './rules/imports'
 
 const config = {
   configs: {
     react,
     node,
+  },
+  rules: {
+    'typescript-react-imports': imports,
   },
 }
 
