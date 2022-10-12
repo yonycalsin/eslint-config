f0f138607a4c9b6fbfd6b322d19997c4966d1ac8
Yony Calsin <58490737+yonycalsin@users.noreply.github.com>
2022-10-12 18:00:55 -0500

    docs(changeset): return the rules first

--- packages/eslint-plugin-import-sort/src/main.ts
+++ packages/eslint-plugin-import-sort/src/main.ts
@@ -3,12 +3,12 @@ import react from './react'
 import imports from './rules/imports'
 
 const config = {
+  rules: {
+    'typescript-react-imports': imports,
+  },
   configs: {
     react,
     node,
-  },
-  rules: {
-    'typescript-react-imports': imports,
   },
 }
 
