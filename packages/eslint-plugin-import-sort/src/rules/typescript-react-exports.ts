a437b34cc4e93dc8344b23fe0b6a45d228df4130
Yony Calsin <helloyonycalsin@gmail.com>
2022-10-12 20:25:30 -0500

    feat: add typescript-react-exports rule

---
+++ packages/eslint-plugin-import-sort/src/rules/typescript-react-exports.ts
@@ -0,0 +1,20 @@
+// @ts-expect-error ts(7016)
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import defineRule from './utils'
+
+export = defineRule({
+  meta: {
+    type: 'layout',
+    fixable: 'code',
+    schema: [],
+    docs: {
+      url: 'https://github.com/yonycalsin/eslint-config',
+    },
+    messages: {
+      sort: 'Run autofix to sort the imports considering the modules',
+    },
+  },
+  create: function (context) {
+    return simpleImportSort.rules.exports.create(context)
+  },
+})
