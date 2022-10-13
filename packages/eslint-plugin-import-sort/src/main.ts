bcf7ed3389284b7eabd30583aef8c4de8fc38983
Yony <58490737+yonycalsin@users.noreply.github.com>
2022-10-12 20:30:03 -0500

    feat: add typescript react exports rule (#21)
    * feat: add typescript-react-exports rule
    
    * docs(changeset): add typescript-react-exports rule
    
    * feat: update the sort message

--- packages/eslint-plugin-import-sort/src/main.ts
+++ packages/eslint-plugin-import-sort/src/main.ts
@@ -1,6 +1,7 @@
 module.exports = {
   rules: {
     'typescript-react-imports': require('./rules/typescript-react-imports'),
+    'typescript-react-exports': require('./rules/typescript-react-exports'),
   },
   configs: {
     react: require('./configs/react'),
