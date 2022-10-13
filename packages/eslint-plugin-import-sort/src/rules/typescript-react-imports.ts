8988c4708ff3b04c93c542b4a9b65ab7eb1fd09b
Yony Calsin <helloyonycalsin@gmail.com>
2022-10-12 20:12:30 -0500

    fix: TypeError: context.getSourceCode is not a function

--- packages/eslint-plugin-import-sort/src/rules/typescript-react-imports.ts
+++ packages/eslint-plugin-import-sort/src/rules/typescript-react-imports.ts
@@ -35,9 +35,8 @@ export = defineRule({
 
     const modifiedContext = Object.assign({}, context, {
       options: [{ groups }],
+      getSourceCode: context.getSourceCode,
     })
-
-    console.log({ originalContext: context, modifiedContext })
 
     return simpleImportSort.rules.imports.create(modifiedContext)
   },
